ad90beef36b96020a4328e2dd60a15ab673b7861
wjcIvan <2065014322@qq.com>
2018-03-28 18:08:16 +0800

    Final Commit

---
+++ app/src/main/java/wjc/ivan/coolweather/db/County.java
@@ -0,0 +1,46 @@
+package wjc.ivan.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 龙心诚 on 2018/3/28 0028.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
---
+++ app/src/main/java/wjc/ivan/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package wjc.ivan.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 龙心诚 on 2018/3/28 0028.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
